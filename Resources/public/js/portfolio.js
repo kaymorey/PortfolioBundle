820e7f2e2382331020fcd5e585c6974a34a2bf85
Katia Moreira <moreirakatia@neuf.fr>
2012-09-12 21:36:32 +0000

    Working on home slider paginator - method changed for backgrounds - added JS effect on buttons

--- Resources/public/js/portfolio.js
+++ Resources/public/js/portfolio.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$("#content #home #paginator .page").click(function() {
 		console.log("ok");
-		$("#content #home #paginator .page img").removeClass("active");
-		$(this).find("img").addClass("active");
+		$("#content #home #paginator .page").removeClass("active");
+		$(this).addClass("active");
 	});
 });
